7237206a84d9e1c10f6484b17ebcfe54c0716df0
cr0sh <ska827@naver.com>
2016-08-08 00:52:25 +0900

    Simplized time format

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/cr0sh/ttfimg"
 )
 
-const width, height = 150, 30
+const width, height = 50, 50
 const fsize, dpi = 15, 72
 
 func main() {
@@ -23,12 +23,7 @@ func main() {
 		t := time.Now()
 		var buf string
 		h, m, s := t.Hour(), t.Minute(), t.Second()
-		buf += fmt.Sprintf("%s %d시 %d분 %d초", func() string {
-			if h >= 12 {
-				return "오후"
-			}
-			return "오전"
-		}(), (h-1)%12+1, m, s)
+		buf += fmt.Sprintf("%02d:%02d\n %02d초", (h-1)%12+1, m, s)
 		rgba := d.Draw(buf)
 		if err := png.Encode(w, rgba); err != nil {
 			log.Printf("PNG encode error: %#v", err)
